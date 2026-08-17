ae58f4406166d95e58342a16d1ef630e715e0b40
amaurygiboudot-arch <amaurygiboudot@gmail.com>
2026-08-17 14:58:22 +0200

    Corriger dépendance du recadrage image

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ android {
 dependencies {
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
 }
 
 kotlin { jvmToolchain(17) }
