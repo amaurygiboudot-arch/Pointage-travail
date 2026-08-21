302e23f0233ea0844bcf0b0e4d39e04ee26b64e7
amaurygiboudot-arch <amaurygiboudot@gmail.com>
2026-08-22 01:01:56 +0200

    Move and rename Google account button

--- app/src/main/java/com/amaury/pointage/FirebaseAccountButtonView.kt
+++ app/src/main/java/com/amaury/pointage/FirebaseAccountButtonView.kt
@@ -3,6 +3,7 @@ package com.amaury.pointage
 import android.content.Context
 import android.content.Intent
 import android.util.AttributeSet
+import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatButton
 
 class FirebaseAccountButtonView @JvmOverloads constructor(
@@ -12,8 +13,21 @@ class FirebaseAccountButtonView @JvmOverloads constructor(
 ) : AppCompatButton(context, attrs, defStyleAttr) {
 
     init {
+        text = "COMPTE GOOGLE"
         setOnClickListener {
             context.startActivity(Intent(context, FirebaseAccountActivity::class.java))
+        }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        post {
+            val container = parent as? ViewGroup ?: return@post
+            if (container.indexOfChild(this) != container.childCount - 1) {
+                val params = layoutParams
+                container.removeView(this)
+                container.addView(this, params)
+            }
         }
     }
 }
