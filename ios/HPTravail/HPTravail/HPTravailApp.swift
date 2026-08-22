de9628bae180500383ec473259fd54d5c3ef88e8
amaurygiboudot-arch <amaurygiboudot@gmail.com>
2026-08-22 14:57:39 +0200

    iOS: initialiser authentification Firebase

--- ios/HPTravail/HPTravail/HPTravailApp.swift
+++ ios/HPTravail/HPTravail/HPTravailApp.swift
@@ -1,17 +1,23 @@
 import SwiftUI
+import GoogleSignIn
 
 @main
 struct HPTravailApp: App {
     @StateObject private var store = WorkStore()
     @StateObject private var locationManager = LocationManager()
+    @StateObject private var authManager = AuthManager()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(store)
                 .environmentObject(locationManager)
+                .environmentObject(authManager)
                 .onAppear {
                     locationManager.requestWhenInUseIfNeeded()
+                }
+                .onOpenURL { url in
+                    GIDSignIn.sharedInstance.handle(url)
                 }
         }
     }
