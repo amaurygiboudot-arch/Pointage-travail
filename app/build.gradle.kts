5a0c6bc796ed895d40cd4860ac1d4fb3258a8a42
amaurygiboudot-arch <amaurygiboudot@gmail.com>
2026-09-02 04:51:10 +0200

    Activer le desugaring java.time pour Android 23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,10 +61,12 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 }
 
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
     implementation(platform("com.google.firebase:firebase-bom:34.17.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
