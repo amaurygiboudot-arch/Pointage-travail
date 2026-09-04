d61b999d14e2fea88b5f2619b98c1909dbdbb631
amaurygiboudot-arch <amaurygiboudot@gmail.com>
2026-09-04 16:14:18 +0200

    Brancher ANI 2.2 sur le minimum employeur

--- app/src/main/java/com/amaury/pointage/v2/engine/NetSalaryEngineV2.kt
+++ app/src/main/java/com/amaury/pointage/v2/engine/NetSalaryEngineV2.kt
@@ -40,7 +40,13 @@ object NetSalaryEngineV2 {
         )
         val statutory = SocialContributionCatalogV2.estimateEmployeeDeductions(gross, year, ceiling)
         val retirement = ComplementaryRetirementCatalogV2.estimate(gross, year, company.professionalStatus, ceiling)
-        val statusContributions = ProfessionalStatusContributionCatalogV2.estimate(gross, year, company.professionalStatus, ceiling)
+        val statusContributions = ProfessionalStatusContributionCatalogV2.estimate(
+            gross = gross,
+            year = year,
+            professionalStatus = company.professionalStatus,
+            ceiling = ceiling,
+            protectionCategory = company.protectionCategory
+        )
         val conventionProvident = ConventionProvidentCatalogV2.estimate(
             gross = gross,
             year = year,
