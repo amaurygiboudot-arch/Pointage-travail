9ad794f57aba0bafc57e05b5dc2271008f843d51
amaurygiboudot-arch <amaurygiboudot@gmail.com>
2026-09-05 21:59:31 +0200

    Corriger le nom de route BOCC PISTE

--- functions/index.js
+++ functions/index.js
@@ -21,7 +21,7 @@ const ALLOWED_PATHS = new Set([
   "/list/conventions",
   "/consult/getArticle",
   "/consult/legiPart",
-  "/list/boccsAndTexts",
+  "/list/boccAndTexts",
   "/consult/getBoccTextPdfMetadata",
   "/consult/lastNJo",
   "/consult/jorfCont",
