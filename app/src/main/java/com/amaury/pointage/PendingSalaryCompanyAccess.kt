f4582b0be01ff74e9b4d65f06d20b17340626e5f
amaurygiboudot-arch <amaurygiboudot@gmail.com>
2026-09-13 20:19:52 +0200

    refactor: isoler l'accès temporaire Salaire V2

---
+++ app/src/main/java/com/amaury/pointage/PendingSalaryCompanyAccess.kt
@@ -0,0 +1,8 @@
+package com.amaury.pointage
+
+/**
+ * Passage temporaire de l'entreprise authentifiée entre SalaryAuthActivity et l'interface Salaire V2.
+ */
+object PendingSalaryCompanyAccess {
+    var authorizedCompanyId: String? = null
+}
