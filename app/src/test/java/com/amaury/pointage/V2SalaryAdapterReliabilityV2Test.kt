dc1e6390306bedab0deade5e84aca1b95d2814c1
amaurygiboudot-arch <amaurygiboudot@gmail.com>
2026-09-15 16:28:39 +0200

    test(v2): documenter la régression du brut provisoire

--- app/src/test/java/com/amaury/pointage/V2SalaryAdapterReliabilityV2Test.kt
+++ app/src/test/java/com/amaury/pointage/V2SalaryAdapterReliabilityV2Test.kt
@@ -4,6 +4,7 @@ import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
+/** Régression : un barème provisoire réellement utilisé ne doit jamais certifier le brut mensuel. */
 class V2SalaryAdapterReliabilityV2Test {
     @Test
     fun provisionalComplementaryRateMakesMonthlyGrossUnreliable() {
