c576698091a50b35b45c960a03172237ecf42a47
amaurygiboudot-arch <amaurygiboudot@gmail.com>
2026-09-19 06:45:23 +0200

    security: constrain build classpath to patched Bouncy Castle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,31 @@
+buildscript {
+    dependencies {
+        constraints {
+            add("classpath", "org.bouncycastle:bcprov-jdk18on") {
+                version {
+                    strictly("[1.85,2.0[")
+                    prefer("1.85.2")
+                }
+                because("CVE-2026-8763 and CVE-2026-13506 affect Bouncy Castle versions before 1.85")
+            }
+            add("classpath", "org.bouncycastle:bcpkix-jdk18on") {
+                version {
+                    strictly("[1.85,2.0[")
+                    prefer("1.85")
+                }
+                because("Keep the Android build classpath on the patched Bouncy Castle 1.85 family")
+            }
+            add("classpath", "org.bouncycastle:bcutil-jdk18on") {
+                version {
+                    strictly("[1.85,2.0[")
+                    prefer("1.85.1")
+                }
+                because("Keep the Android build classpath on the patched Bouncy Castle 1.85 family")
+            }
+        }
+    }
+}
+
 pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
 rootProject.name = "PointageTravail"
