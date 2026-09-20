e0036f6c7fd3da736b0f4b340668220c6dc37d9a
amaurygiboudot-arch <amaurygiboudot@gmail.com>
2026-09-20 21:09:42 +0200

    Keep RuntimeV2 independent from SalaryV2

--- ios/HPTravail/Package.swift
+++ ios/HPTravail/Package.swift
@@ -17,12 +17,11 @@ let package = Package(
         ),
         .target(
             name: "RuntimeV2Contract",
-            dependencies: ["SalaryV2Contract"],
             path: "HPTravail/RuntimeV2"
         ),
         .testTarget(
             name: "SalaryV2ContractTests",
-            dependencies: ["SalaryV2Contract", "RuntimeV2Contract"],
+            dependencies: ["SalaryV2Contract"],
             path: "HPTravailTests"
         ),
         .testTarget(
