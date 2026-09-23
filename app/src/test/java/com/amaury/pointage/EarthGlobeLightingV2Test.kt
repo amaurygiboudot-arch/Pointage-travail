76d275c85739b14e5bae274b0e145ae0d988709b
amaurygiboudot-arch <amaurygiboudot@gmail.com>
2026-09-23 07:14:12 +0200

    Test progressive day night globe transition

--- app/src/test/java/com/amaury/pointage/EarthGlobeLightingV2Test.kt
+++ app/src/test/java/com/amaury/pointage/EarthGlobeLightingV2Test.kt
@@ -7,12 +7,14 @@ class EarthGlobeLightingV2Test {
     @Test
     fun `sunlit face is visibly brighter than night face`() {
         val night = earthSunBrightnessV2(-1.0, 1.0)
+        val twilight = earthSunBrightnessV2(-0.05, 1.0)
         val terminator = earthSunBrightnessV2(0.0, 1.0)
         val daylight = earthSunBrightnessV2(0.5, 1.0)
         val directSun = earthSunBrightnessV2(1.0, 1.0)
 
-        assertTrue(night <= terminator)
-        assertTrue(daylight > terminator * 3.0)
+        assertTrue(night < twilight)
+        assertTrue(twilight < terminator)
+        assertTrue(daylight > terminator * 2.0)
         assertTrue(directSun > daylight)
         assertTrue(directSun > 1.0)
     }
